68250c70440deabb92f12285f96c0ecd278c9be1
Sanjay-D-works <Sanjaydevarajworks@gmail.com>
2025-05-30 22:50:39 +0530

    Created an computer guessing the number

--- Guess_the_Number_1.py
+++ Guess_the_Number_1.py
@@ -1,1 +1,18 @@
+#Guess the number (Computer)
 
+import random 
+
+def guess(X):
+
+    random_number = random.randint(1,X)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Enter the number 1 to {X}: "))
+        if guess < random_number:
+            print("Sorry, guess again, Too low")
+        elif guess > random_number:
+            print("Sorry, guess again,Too high")
+    
+    print(f"Yay, congrats. You have guessed the number {random_number}")
+
+guess(10)
