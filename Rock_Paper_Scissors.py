3bca91c8082cba20f9bedc737aa9eedbdeeb732f
Sanjay-D-works <Sanjaydevarajworks@gmail.com>
2025-06-06 23:44:19 +0530

    write an code for Rock_Paper_Scissors.

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,1 +1,23 @@
-def Rock_paper():
+# Rock Paper Scissors
+
+import random
+
+def play():
+    user = input("What's your choice? 'r' for Rock, 'p' for Paper, 's' for Scissors\n")
+    computer = random.choice(['r','p','s'])
+    
+    if user == computer:
+        return 'It\'s is a Tie'
+        
+    if is_win(user, computer):
+        return 'You Win!'
+        
+    return 'You Lose!'
+    
+def is_win(player,opponent):
+        #return true if player is wins
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+            
+            
+print(play())
