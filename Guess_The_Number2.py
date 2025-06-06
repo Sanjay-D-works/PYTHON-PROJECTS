83065d178c602a89ec4d0d0355ae70938c69724e
Sanjay-D-works <Sanjaydevarajworks@gmail.com>
2025-06-06 23:30:49 +0530

    write an code for Guess_The_Number using computer

--- Guess_The_Number2.py
+++ Guess_The_Number2.py
@@ -1,3 +1,20 @@
-# Guessing the number 
+# Guessing the number (computer)
 
-def Guess_the_number():
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low  # could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (c)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f'Yay! The Computer guessed your number, {guess}, Correctly!')            
+    
+computer_guess(10)
+
