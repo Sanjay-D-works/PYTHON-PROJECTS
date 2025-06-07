8bfff552b77381cf80958c7bf751b0427a483497
Sanjay-D-works <Sanjaydevarajworks@gmail.com>
2025-06-07 23:31:02 +0530

    Write an code for Hangman code for this project

--- Hangman.py
+++ Hangman.py
@@ -1,1 +1,41 @@
+import random
+from words import words
+import string
 
+def get_valid_word(words):
+    word = random.choice(words) #randomly choose something from the json file.
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+        
+    return words
+    
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)  #letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() # what the user has guessed
+    
+    #Getting user input
+    while len(word_letters) > 0:
+        #letters used
+        #' '.join(['a','b','c' --> 'a b cd'])
+        print('You have used these letters: ',' '.join(used_letters))
+        
+        #What current word is (ie: W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ',' '.join(word_list))
+        
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            
+        elif user_letter in used_letters:
+            print("You have already used that character. Please try again.")
+        
+        else:
+            print("Invalid character. Please try again.")
+    
+    #Gets here when len(word_letters) == 0.
+user_input = input()
