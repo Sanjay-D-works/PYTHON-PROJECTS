a84d96e2dc24f2583fcfbbfccee4f000bb6ced8b
Sanjay-D-works <Sanjaydevarajworks@gmail.com>
2025-06-09 22:45:30 +0530

    Write an code for hangman and update extra methods in this program

--- Hangman.py
+++ Hangman.py
@@ -15,11 +15,13 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
     
+    lives = 6
+    
     #Getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         #letters used
         #' '.join(['a','b','c' --> 'a b cd'])
-        print('You have used these letters: ',' '.join(used_letters))
+        print('you have', lives , 'lives left and You have used these letters: ',' '.join(used_letters))
         
         #What current word is (ie: W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
@@ -30,6 +32,10 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+                
+            else:
+                lives = lives - 1 # Takes away a life if wrong
+                print("Letter is not in word.")
             
         elif user_letter in used_letters:
             print("You have already used that character. Please try again.")
@@ -37,5 +43,11 @@ def hangman():
         else:
             print("Invalid character. Please try again.")
     
-    #Gets here when len(word_letters) == 0.
-user_input = input()
+    #Gets here when len(word_letters) == 0 OR when lives == 0.
+    if lives == 0:
+        print("You died ,Sorry. The Word was", word)
+    else:
+        print('You guessed the word', word,'!!')
+    
+    
+hangman()
