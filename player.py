fca970639edd2b4dceae3d1edf282797c6951509
Sanjay-D-works <Sanjaydevarajworks@gmail.com>
2025-06-22 21:03:17 +0530

    Debugging the player.py file

--- player.py
+++ player.py
@@ -1,22 +1,18 @@
-import math
 import random
 
 class Player:
     def __init__(self, letter):
-        #letter is x or o
         self.letter = letter
 
-    #we want all players to get their next move
-    def ger_move(self, game):
+    def get_move(self, game):
         pass
 
 class RandomComputerPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
-    def ger_move(self, game):
-        square = random.choice(game.available_moves())
-        return square
+    def get_move(self, game):
+        return random.choice(game.available_moves())
 
 class HumanPlayer(Player):
     def __init__(self, letter):
@@ -26,15 +22,12 @@ class HumanPlayer(Player):
         valid_square = False
         val = None
         while not valid_square:
-            square = input(self.letter + '\'s turn. Input move(0-9):')
-            # We're going to check that this is a correct value by trying to cast
-            # it to an interger, and if it's not, then we say its invalid
-            #if that spot is not available on the board, we also say its invalid
-
+            square = input(self.letter + "'s turn. Input move (0-8): ")
             try:
                 val = int(square)
                 if val not in game.available_moves():
                     raise ValueError
+                valid_square = True
             except ValueError:
                 print("Invalid square. Try again.")
 
