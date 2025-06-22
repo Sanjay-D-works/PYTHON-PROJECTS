b5e4b29cbf3c3e34531a33a9d888f98699804017
Sanjay-D-works <Sanjaydevarajworks@gmail.com>
2025-06-22 21:01:57 +0530

    Debugging the game.py file

--- game.py
+++ game.py
@@ -1,20 +1,20 @@
+import time 
+from player import HumanPlayer, RandomComputerPlayer
+
 class TicTacToc:
     def __init__(self):
-        self.board = [' ' for _ in range(9)] #we will use a single list to rep 3*3 board
-        self.current_winner = None #keep track of winner!
+        self.board = [' ' for _ in range(9)]
+        self.current_winner = None
 
     def print_board(self):
-        #This  is just getting the rows
         for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
-            print('| ' * '| '.join(row) + ' |')
+            print('| ' + ' | '.join(row) + ' |')
 
     @staticmethod
     def print_board_nums():
-        # 0 | 1 | 2 etc (tells us what number corresponds to what box)
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
-            print('| ' * '| '.join(row) + ' |')
-
+            print('| ' + ' | '.join(row) + ' |')
 
     def available_moves(self):
         return [i for i, spot in enumerate(self.board) if spot == ' ']
@@ -25,31 +25,50 @@ class TicTacToc:
     def num_empty_squares(self):
         return self.board.count(' ')
     
-    def make_move(self, square, letter):
+    def make_move(self, square, letter): 
         if self.board[square] == ' ':
             self.board[square] = letter
-            if self.wimmer(square, letter):
+            if self.winner(square, letter):
                 self.current_winner = letter
             return True
         return False
        
+    def winner(self, square, letter):
+        row_ind = square // 3
+        row = self.board[row_ind * 3 : (row_ind + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
+        
+        col_ind = square % 3
+        column = [self.board[col_ind + i * 3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True
+        
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            if all([spot == letter for spot in diagonal2]):
+                return True
+            
+        return False 
+                  
 
 def play(game, x_player, o_player , print_game=True):
     if print_game:
         game.print_board_nums()
 
-    letter = 'X' #strating letter
-    # iterate while the game still has empty square 
+    letter = 'X'
     while game.empty_squares():
         if letter == 'O':
             square = o_player.get_move(game)
         else:
             square = x_player.get_move(game)
 
-
         if game.make_move(square, letter):
             if print_game:
-                print(letter + f'makes a move to square {square}')
+                print(letter + f' makes a move to square {square}')
                 game.print_board()
                 print(' ')
 
@@ -59,11 +78,18 @@ def play(game, x_player, o_player , print_game=True):
                 return letter
 
             letter = 'O' if letter == 'X' else 'X'
-            if letter == 'X':
-                letter = 'O'
-            else:
-                letter = 'X'
 
+        time.sleep(0.8)
+
+    if print_game:
+        print("It's a tie!")
+
+
+if __name__ == '__main__':
+    x_player = HumanPlayer('X')
+    o_player = RandomComputerPlayer('O')
+    t = TicTacToc()
+    play(t, x_player, o_player, print_game=True)
 
         if print_game:
             print('It\'s a tie')
